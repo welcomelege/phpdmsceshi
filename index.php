43ff7dfa1d173f66bc3861a75f367dbf2b3b3b8c
wangjiale <welcome_lege@163.com>
2024-11-15 16:55:05 +0800

    1
    Signed-off-by: wangjiale <welcome_lege@163.com>

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require "vendor/autoload.php";
+
+$Ceshi= new Siam\Ceshi();
+
+$Ceshi->ceshi();
+
+?>
---
+++ src/siam/Ceshi.php
@@ -0,0 +1,19 @@
+<?php
+//Ceshi.php文件
+
+namespace Siam;
+
+class Ceshi
+
+{
+
+    function ceshi()
+
+    {
+
+        echo "Form Ceshi -> ceshi()";
+
+    }
+
+}
+?>
